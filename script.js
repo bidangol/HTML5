70de21f2ae78c436cb6160493af22e2393e6a8fb
bidangol <bikram.dangol@ymail.com>
2022-06-12 18:35:48 -0400

    4 files modified

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('hello!')
+var x = 10;
+var y = 20;
+var z = x + y;
+console.log("The value of z is ", z);
+
+var player1 = new Object();
+player1.name = "Stoick the Vast";
+player1.age = 50;
+player1.maxScore = 1000;
+
+var player2 = {name: "Hiccup Horrendous Haddock III", age: 15, maxScore: 1000};
+
+var currentGamesScores = {};
+
+var highestEverScore = player1.maxScore;
