3d6087d91b13a896aecb30323ce1f6483809b859
bidangol <97066636+bidangol@users.noreply.github.com>
2023-01-07 19:50:05 -0500

    3 files modified

--- hiccupQuiz.js
+++ hiccupQuiz.js
@@ -31,8 +31,32 @@ const questions = [
   },
   {
     id: 6,
-    question: "Is Hiccup main protagonist or antagonist??",
+    question: "Is Hiccup main protagonist or antagonist?",
     options: ['Main protagonist','Main antagonist'],
+    correctAnswer: 0
+  },
+  {
+    id: 7,
+    question: "Is Hiccup early chief or late chief?",
+    options: ['Early cheif','Late cheif'],
+    correctAnswer: 1
+  },
+  {
+    id: 8,
+    question: "Are Night Fury's thor?",
+    options: ['Yes','Simillar (mostly not the same)','No'],
+    correctAnswer: 1
+  },
+  {
+    id: 9,
+    question: "How old is Toothless?",
+    options: ['15-30','20-30'],
+    correctAnswer: 0
+  },
+  {
+    id: 10,
+    question: "Are Night Fury's intelligent?",
+    options: ['Yes','No'],
     correctAnswer: 0
   },
 ];
@@ -60,4 +84,13 @@ const homecoming = document.getElementById("homecoming");
  homecoming.appendChild(createQuestion(4));
 
 const protagonist = document.getElementById("protagonist");
+ protagonist.appendChild(createQuestion(5));
+
+ const earlycheif = document.getElementById("earlychief");
+ earlycheif.appendChild(createQuestion(6));
+
+ const thor = document.getElementById("thor");
+ thor.appendChild(createQuestion(7));
+
+ const protagonist = document.getElementById("protagonist");
  protagonist.appendChild(createQuestion(5));
